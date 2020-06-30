aef0d31a53a8d5bed5f825e8f532d96a0744d022
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-30 17:27:09 -0300

    refactor: create lesson use CreateLessonInput

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Lesson } from './lesson.entity';
 import { Repository } from 'typeorm';
 import * as faker from 'faker';
+import { CreateLessonInput } from './lesson.input';
 
 @Injectable()
 export class LessonService {
@@ -10,7 +11,9 @@ export class LessonService {
     @InjectRepository(Lesson) private lessonRepository: Repository<Lesson>
   ) {}
 
-  async createLesson(name, startDate, endDate): Promise<Lesson> {
+  async createLesson(createLessonInput: CreateLessonInput): Promise<Lesson> {
+    const { name, startDate, endDate } = createLessonInput;
+    
     const lesson = this.lessonRepository.create({
       id: faker.random.uuid(),
       name,
